73014d96784be314cc9db37c73d0ddd6ebf24e43
danielinsuasti <daniel.insuasti@correounivalle.edu.co>
2019-02-26 19:08:26 -0500

    First commit, Until this moment, only the first image in the 'Funders' section has the modal, I need to agree modals to all the funders; I need to link the Blogs'aditional views' with the index page; I need to acquire the goodaddy domain; and I need to save the user info in the LocalStorage

---
+++ script.js
@@ -0,0 +1,161 @@
+let about = document.getElementById("about");
+let getAbout = document.getElementById("getAbout");
+let resume = document.getElementById("resume");
+let getResume = document.getElementById("getResume");
+let works = document.getElementById("works");
+let getWorks = document.getElementById("getWorks");
+let blog = document.getElementById("blog");
+let getBlog = document.getElementById("getBlog");
+let contact = document.getElementById("contact");
+let getContact = document.getElementById("getContact");
+
+//console.log(getResume)
+
+function remove() {
+    about.classList.remove('view');
+    getAbout.classList.remove('selected');
+    resume.classList.remove('view');
+    getResume.classList.remove('selected');
+    works.classList.remove('view');
+    getWorks.classList.remove('selected');
+    blog.classList.remove('view');
+    getBlog.classList.remove('selected');
+    contact.classList.remove('view');
+    getContact.classList.remove('selected');
+}
+
+getAbout.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove('about');
+        about.classList.add('view');
+        getAbout.classList.add('selected');
+    }
+
+});
+getResume.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove();
+        resume.classList.add('view');
+        getResume.classList.add('selected');
+    }
+})
+getWorks.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove();
+        works.classList.add('view');
+        getWorks.classList.add('selected');
+    }
+})
+getBlog.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove();
+        blog.classList.add('view');
+        getBlog.classList.add('selected');
+    }
+})
+getContact.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        remove();
+        contact.classList.add('view');
+        getContact.classList.add('selected');
+    }
+
+})
+
+var email = document.getElementById("email");
+var form = document.getElementById("form");
+
+email.addEventListener("input", function (event) {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity("I expect an e-mail, darling!");
+  } else {
+    email.setCustomValidity("");
+  }
+});
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+  });
+
+
+/*Filter divs section   works -> works-header -> works-content -> line-left*/
+
+filterSelection("all");
+
+function filterSelection(c){
+    var x, i;
+    var x = document.getElementsByClassName("filterFunders");
+    if(c == "all") c ="";
+    // adicionar la clase "show"(display:block) para los elementos filtrados,
+    // y remover la clase "show" de los elementos que no son seleccionados
+    for(i = 0; i < x.length; i++) {
+        w3RemoveClass(x[i], "show");
+        if(x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    }    
+}
+
+//Muestra los elementos filtrados
+function w3AddClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for(i = 0; i < arr2.length; i++){
+        if(arr1.indexOf(arr2[i]) == -1 ){
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+//Oculta los que no son seleccionados.
+function w3RemoveClass(element, name){
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i = 0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1);
+        }
+    }
+    element.className = arr1.join(" ");
+}
+
+//Adiciona clases activas al actual conjunto de Works-header
+
+var worksHeaderContainer = document.getElementById("works");
+var lis = worksHeaderContainer.getElementsByClassName("li");
+
+for (var i = 0; i < lis.length; i++) {
+    lis[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('myImg');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+
